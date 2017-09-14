401be66b25ed1bcd0d29907d976f2c2a69264781
ErbuRB5 <31067794+ErbuRB5@users.noreply.github.com>
2017-09-14 20:59:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,56 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+const prefix = botSettings.prefix;
 
-client.on('ready', () => {
-    console.log('I am ready!');
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+	console.log(`Bot cargado! ${bot.user.username}`);
+	
+	try {
+		let link =  await bot.generateInvite(["ADMINISTRATOR"]);
+		console.log(link);
+	} catch(e) {
+		console.log(e.stack);
+	}
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+bot.on("message", async message => {
+	if(message.author.bot) return;
+	if(message.channel.type ==="dn") return;
+	
+	let messageArray = message.content.split(" ");
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
+	
+	if(!command.startsWith(prefix)) return;
+	
+	if(command === `${prefix}userinfo`) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setDescription("Información del usuario:")
+			.setColor("#9B59B6")
+			.addField("Nombre de usuario:", `${message.author.username}#${message.author.discriminator}`)
+			.addField("ID", message.author.id)
+			.addField("Se unió el:", message.author.createdAt);
+		
+		message.channel.sendEmbed(embed);
+		
+		return;
+	}
+	if(command === `${prefix}help`) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setDescription(":theinfinity: The Infinity Bot Help!")
+			.setColor("#9B59B6")
+			.addField("Nombre de usuario:", `${message.author.username}#${message.author.discriminator}`)
+			.addField("ID", message.author.id)
+			.addField("Se unió el:", message.author.createdAt);
+		
+		message.channel.sendEmbed(embed);
+		
+		return;
+	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
